9de141daeabae8400edf5d643d3975b461a8a160
Ivan Pedrazas <ipedrazas@gmail.com>
2012-09-12 17:44:58 +0100

    moved to prod

--- app/services.js
+++ app/services.js
@@ -1,64 +1,65 @@
 App.Services = (function(lng, app, undefined) {
 
-	var API_URL = "http://api.blibb.it/ipedrazas/bugalive";
-	var PARAMETERS = {
-		callback: '?' // jsonp
-	};
+    var API_URL = "http://api.blibb.net/ipedrazas/bugalive-com";
+    var PARAMETERS = {
+        callback: '?' // jsonp
+    };
 
-	var bug_service = function() {
-		fetch_data('bug', PARAMETERS, '#bugs-list');
-	};
+    var bug_service = function() {
+        fetch_data('bug', PARAMETERS, '#bugs-list');
+    };
 
-	var critical_service = function(){
-		fetch_data('critical', PARAMETERS, '#critical-list');
-	};
+    var critical_service = function(){
+        fetch_data('critical', PARAMETERS, '#critical-list');
+    };
 
-	var enhancement_service = function(){
-		fetch_data('enhancement', PARAMETERS, '#enhancement-list');
-	};
+    var enhancement_service = function(){
+        fetch_data('enhancement', PARAMETERS, '#enhancement-list');
+    };
 
-	function fetch_data(level, params, element_id){
-		var url = API_URL + "/tag/" + level;
-		lng.Service.get(url, params, function(response) {
-			app.View.bug_view(element_id, response.items);
-		});
-	};
+    function fetch_data(level, params, element_id){
+        var url = API_URL + "/tag/" + level;
+        lng.Service.get(url, params, function(response) {
+            app.View.bug_view(element_id, response.items);
+        });
+    }
 
-	var add_bug = function(){
+    var add_bug = function(){
 
-		// var post_params = {
-		//         key: "bugalive.com",
-		//         app_token: "bugalive.com",
-		//         '01-title': lng.dom('#bug_title').val(),
-		//         '02-bug': lng.dom('#bug_description').val(),
-		//         '03-date': lng.dom('#bug_date').val(),
-		//         '01-status': 'active',
-		//         tags: lng.dom('#bug_type').val()
-		//     };
+        // var post_params = {
+        //         key: "bugalive.com",
+        //         app_token: "bugalive.com",
+        //         '01-title': lng.dom('#bug_title').val(),
+        //         '02-bug': lng.dom('#bug_description').val(),
+        //         '03-date': lng.dom('#bug_date').val(),
+        //         '01-status': 'active',
+        //         tags: lng.dom('#bug_type').val()
+        //     };
 
-		var params = "key=bugalive.com&app_token=bugalive.com&01-title="+ lng.dom('#bug_title').val() 
-				+"&02-bug="+ lng.dom('#bug_description').val() +"&03-date="+ lng.dom('#bug_date').val() +"&01-status=active&tags=" + lng.dom('#bug_type').val();
 
-		lng.Service.post(API_URL, params, function(response) {
-			refresh();
-			cleanup();
-		});
-	};
+        var params = "key=bugalive-com&app_token=bugalive.com&01-title=" + lng.dom('#bug_title').val() +
+             "&bug=" + lng.dom('#bug_description').val() + "&date=" + lng.dom('#bug_date').val() + "&assigment=" + lng.dom('#bug_assign').val() + "&status=active&tags=" + lng.dom('#bug_type').val();
 
-	function refresh(){
-		bug_service();
-		critical_service();
-		enhancement_service();
-	};
+        lng.Service.post(API_URL, params, function(response) {
+            refresh();
+            cleanup();
+        });
+    };
 
-	function cleanup(){
-		lng.dom('#bug_description').val("");
-		lng.dom('#bug_title').val(""); 
-	};
-	
-	return {
-		add_bug: add_bug,
-		refresh: refresh
-	}
+    function refresh(){
+        bug_service();
+        critical_service();
+        enhancement_service();
+    }
+
+    function cleanup(){
+        lng.dom('#bug_description').val("");
+        lng.dom('#bug_title').val("");
+    }
+
+    return {
+        add_bug: add_bug,
+        refresh: refresh
+    };
 
 })(LUNGO, App);
